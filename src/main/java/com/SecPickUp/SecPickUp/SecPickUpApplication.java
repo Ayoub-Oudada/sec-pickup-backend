68766684f5a41104e770314f53005bb2bd7e5d22
HMOUDAT OUSSAMA <hmoudat.oussama@gmail.com>
2023-11-23 16:18:20 +0100

    Ajout du nouveau dossier Spring Boot operation 2

---
+++ src/main/java/com/SecPickUp/SecPickUp/SecPickUpApplication.java
@@ -0,0 +1,13 @@
+package com.SecPickUp.SecPickUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecPickUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecPickUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SecPickUp/SecPickUp/SecPickUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SecPickUp.SecPickUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecPickUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
