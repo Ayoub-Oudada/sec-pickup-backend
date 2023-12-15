851b3fdb2e02a4087d4dccbde2c52f2a7195cea2
ZakaeiaeSAIDI <zakariae.saidi@uit.ac.ma>
2023-12-16 00:24:19 +0100

    CRUD user and Authentification logique

---
+++ src/main/java/com/backend/services/UsersService.java
@@ -0,0 +1,82 @@
+package com.backend.services;
+
+import com.backend.controllers.UsersController;
+import com.backend.entities.User;
+import com.backend.entities.UserAccountType;
+import com.backend.repositories.UsersRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+public class UsersService {
+    private final UsersRepository usersRepository;
+
+    @Autowired
+    public UsersService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    public List<User> GetUsers(){
+        return usersRepository.findAll();
+
+    }
+    public void addNewUser(User user){
+        Optional<User> studentOptional=usersRepository
+                .findUserByUsername(user.getUsername());
+        if(studentOptional.isPresent()){
+            throw new IllegalStateException("Username taken");
+        }
+        usersRepository.save(user);
+
+    }
+
+    public void deleteUser(Long userId) {
+        boolean exists =usersRepository.existsById(userId);
+        if(!exists){
+            throw new IllegalStateException("User with id "+userId + "does not exists");
+        }
+        usersRepository.deleteById(userId);
+    }
+    @Transactional
+    public void updateUser(Long userId, String username, String password) {
+        User user = usersRepository.findById(userId)
+                .orElseThrow(()->new IllegalStateException("user with id "+ userId+" does not existe"));
+
+        if(username !=null &&
+                username.length()>0 &&
+                !Objects.equals(user.getUsername(),username)){
+            Optional<User> userOptional =usersRepository.findUserByUsername(username);
+            if(userOptional.isPresent()){
+                throw new IllegalStateException("username taken");
+            }
+            user.setUsername(username);
+        }
+
+        if(password !=null &&
+                password.length()>0 &&
+                !Objects.equals(user.getPassword(),password)){
+
+
+            user.setPassword(password);
+        }
+    }
+
+    public User SearchUser(String password, String username,UserAccountType type ) {
+
+        Optional<User> userOptional =usersRepository.authenticate(password,username,type);
+        if( userOptional.isPresent()){
+            User user = userOptional.get();
+            System.out.println("Authentification OK");
+            return user;
+            //
+        }else{throw new IllegalStateException("User does not exists");}
+
+
+    }
+
+}
